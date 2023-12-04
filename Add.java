1ebc8c399fa20a413c4ca98f7062ec08ef22f1ca
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-04 13:43:25 +0900

    addition

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add {
+
+    public static void main(String[] args) {
+
+        int first = 10;
+        int second = 30;
+
+        int product = first + second;
+
+        System.out.println("The product is: " + product);
+    }
+}
