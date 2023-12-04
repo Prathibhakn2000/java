8e49023e0dfef44f1a9ef17289429df091bfd218
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-04 14:17:18 +0900

    datatypes

---
+++ Numerical.java
@@ -0,0 +1,248 @@
+public class Numerical {
+	public static void main(String [] args){
+		
+	  ///float///
+		
+	 float height = 5.2f;
+	 System.out.println(height);
+	 height = 5.5f;
+	 System.out.println(height);
+		
+	 float radiusOfEarth = 6.37f;
+	 System.out.println(radiusOfEarth);
+		
+	 float valueOfPie = 3.14f;
+	 System.out.println(valueOfPie);
+		
+	 float bankBalance = 50.345f;
+	 System.out.println(bankBalance);
+	 bankBalance = 80.096f;
+	 System.out.println(bankBalance);
+		
+	 float cgpa = 9.5f;
+	 System.out.println(cgpa);
+		
+	 float mobileData = 1.5f;
+	 System.out.println(mobileData);
+	 mobileData = 2.0f;
+	 System.out.println(mobileData);
+		
+	 float exponentialConstant = 2.718f;
+	 System.out.println(exponentialConstant);
+		
+	 float constantOfGravitation = 6.67f;
+	 System.out.println(constantOfGravitation);
+		
+	 float weight = 50.7f;
+	 System.out.println(weight);
+	 weight = 52.9f;
+	 System.out.println(weight);
+		
+	 float petrolPrice = 101.94f;
+	 System.out.println(petrolPrice);
+		
+	 float appStorage = 12.5f;
+	 System.out.println(appStorage);
+	 appStorage = 11.4f;
+	 System.out.println(appStorage);
+		
+	 float audioSeed = 1.5f;
+	 System.out.println(audioSeed);
+		
+	 float kilometer = 1.5f;
+	 System.out.println(kilometer);
+	 kilometer = 2.5f;
+	 System.out.println(kilometer);
+		
+	 float amount = 5.5f;
+	 System.out.println(amount);
+		
+	 float instaFollowers = 1.5f;
+	 System.out.println(instaFollowers);
+	 instaFollowers = 1.2f;
+	 System.out.println(instaFollowers);
+		
+	 float sgpa = 8.90f;
+	 System.out.println(sgpa);
+	 sgpa = 9.03f;
+	 System.out.println(sgpa);
+		
+	 float dieselPrice = 87.89f;
+	 System.out.println(dieselPrice);
+		
+		
+	   ///double///
+		
+	 double plancksConstant = 6.626070040d;
+	 System.out.println(plancksConstant);
+		
+	 double faradayConstant = 9.648533289d;
+	 System.out.println(faradayConstant);
+	 
+	 double CircumferenceOfCircle = 6.283185307d;
+     System.out.println(CircumferenceOfCircle);
+
+     double speedOfLight = 2.99792458d;
+     System.out.println(speedOfLight);
+
+     double electronRestMass = 9.10938356d;
+     System.out.println(electronRestMass);
+
+     double redbergConstant = 1.0973731568505d;
+     System.out.println(redbergConstant);
+
+     double protonRestMass = 1.672621898d;
+     System.out.println(protonRestMass);
+
+     double chargeOnElectron = 1.6021766208d;
+     System.out.println(chargeOnElectron);
+
+     double neutronRestMass = 1.674927471d;
+     System.out.println(neutronRestMass);
+
+     double boltzmann = 1.38064852d;
+     System.out.println(boltzmann);
+
+     double plancksConstan = 6.626070040d;
+     System.out.println(plancksConstan);
+
+     double gravitationConstant = 6.6743014d;
+     System.out.println(gravitationConstant);
+
+     double fineStructureConstant = 7.2973525664d;
+     System.out.println(fineStructureConstant);
+
+		
+		
+	   ///char///
+		
+	 char female = 'F';
+	 System.out.println(female);
+		
+	 char male = 'M';
+	 System.out.println(male);
+		
+	 char grade = 'A';
+	 System.out.println(grade);
+	 grade = 'B';
+	 System.out.println(grade);
+		
+	 char result = 'P';
+	 System.out.println(result);
+	 result = 'F';
+     System.out.println(result);
+		
+	 char length = 'l';
+	 System.out.println(length);
+		
+	 char radius = 'r';
+	 System.out.println(radius);
+		
+	 char gravitation = 'g';
+	 System.out.println(gravitation);
+		
+	 char yes = 'Y';
+	 System.out.println(yes);
+		
+	 char no = 'N';
+	 System.out.println(no);
+		
+	 char nameInitial = 'K';
+	 System.out.println(nameInitial);
+	 nameInitial = 'B';
+	 System.out.println(nameInitial);
+		
+	 char massOfEarth = 'M';
+	 System.out.println(massOfEarth);
+		
+	 char meters = 'm';
+	 System.out.println(meters);
+		
+	 char minutes = 'm';
+	 System.out.println(minutes);
+		
+	 char  volts = 'v';
+	 System.out.println(volts);
+		
+	 char oxygen = 'O';
+	 System.out.println(oxygen);
+		
+     char carbon = 'C';
+	 System.out.println(carbon);
+		
+	 char firstLetterOfVowels = 'a';
+	 System.out.println(firstLetterOfVowels);
+        
+     char asciiValueOfTen = 'A';
+	 System.out.println(asciiValueOfTen);
+        
+	  
+	  ///boolean///
+		
+	 boolean javaIsEasy = true;
+	 System.out.println(javaIsEasy);
+	 javaIsEasy = false;
+	 System.out.println(javaIsEasy);
+		
+	 boolean toIsMonday = false;
+	 System.out.println(toIsMonday);
+	 toIsMonday = true;
+	 System.out.println(toIsMonday);
+		
+	 boolean isIndiaWonMatch = false;
+	 System.out.println(isIndiaWonMatch);
+		
+	 boolean isMscComplete = true;
+	 System.out.println(isMscComplete);
+		
+	 boolean thisIsDecemberMonth = true;
+	 System.out.println(thisIsDecemberMonth);
+		
+	 boolean isOneWeekHasSevenDays = true;
+	 System.out.println(isOneWeekHasSevenDays);
+		
+	 boolean sharksAreMammals=false;
+     System.out.println(sharksAreMammals);
+
+     boolean batsAreBlind = false;
+     System.out.println(batsAreBlind);
+
+     boolean southAfricaHasOneCapital = true;
+     System.out.println(southAfricaHasOneCapital);
+
+     boolean carnivoresAreFleshEatingAnimals = true;
+     System.out.println(carnivoresAreFleshEatingAnimals);
+
+     boolean octopusHasTwoHeart = false;
+     System.out.println(octopusHasTwoHeart);
+     
+     boolean indianPeacockIsTheNationalBirdOfIndia = true;
+     System.out.println(indianPeacockIsTheNationalBirdOfIndia);
+
+     boolean humanBodyHasTwoLungs = true;
+     System.out.println(humanBodyHasTwoLungs);
+
+     boolean humanBrainIsMadeOfMuscle = false;
+     System.out.println(humanBrainIsMadeOfMuscle);
+	 
+	 boolean humanBodyIsAboutSixtyPercentageWater=true;
+     System.out.println(humanBodyIsAboutSixtyPercentageWater);
+
+     boolean commonBloodTypeIsONegative=true;
+     System.out.println(commonBloodTypeIsONegative);
+     
+	 boolean fortuneCookiesWereInventedInChina = false;
+     System.out.println(fortuneCookiesWereInventedInChina);
+     
+     boolean vaticanCityIsTheSmallestCityInTheWorld=true;
+     System.out.println(vaticanCityIsTheSmallestCityInTheWorld);
+
+     boolean hummingBirdIsTheWorldsSmallestBird=true;
+     System.out.println(hummingBirdIsTheWorldsSmallestBird);
+	 
+	 boolean hadYourBreakfast = true;
+	 System.out.println(hadYourBreakfast);
+	 hadYourBreakfast = false;
+	 System.out.println(hadYourBreakfast);
+	}
+}
