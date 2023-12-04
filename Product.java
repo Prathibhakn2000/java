3a71b7f3c0b35353014919fa5d5c84d7b5542bb5
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-04 20:31:20 +0900

    datatype

---
+++ Product.java
@@ -0,0 +1,192 @@
+public class Product {
+
+    public static void main(String[] args) {
+		
+	 ///byte///	
+
+     byte numberOfSemestersInDiploma = 6;
+	 System.out.println(numberOfSemestersInDiploma);
+		
+	 byte daysInWeek = 7;
+	 System.out.println(daysInWeek);
+	
+	 byte heightOfPerson = 5;
+     System.out.println(heightOfPerson);
+	 heightOfPerson = 6;
+	 System.out.println(heightOfPerson);
+		
+	 byte numberOfStudentInAClass = 60;
+	 System.out.println(numberOfStudentInAClass);
+	 numberOfStudentInAClass = 100;
+	 System.out.println(numberOfStudentInAClass);
+		
+	 byte numberOfMoviesWatched = 110;
+	 System.out.println(numberOfMoviesWatched);
+		
+	 byte numberOfMinutesInAHour = 60;
+	 System.out.println(numberOfMinutesInAHour);
+		
+	 byte weightOfAPerson = 100;
+	 System.out.println(weightOfAPerson);
+	 weightOfAPerson = 60;
+	 System.out.println(weightOfAPerson);
+		
+	 byte numberOfTaluksInBangaloreRuralDistrict = 4;
+	 System.out.println(numberOfTaluksInBangaloreRuralDistrict);
+		
+	 byte numberOfColoursInRainbow = 7;
+	 System.out.println(numberOfColoursInRainbow);
+		
+	 byte howManyTimesHeartBeatsForAMinute = 60;
+	 System.out.println(howManyTimesHeartBeatsForAMinute);
+	 howManyTimesHeartBeatsForAMinute = 62;
+	 System.out.println(howManyTimesHeartBeatsForAMinute);
+		
+	 byte age = 22;
+     System.out.println(age);
+	 age=23;
+	 System.out.println(age);
+		
+	 byte numberOfSubjectsPassedInASemester = 6;
+     System.out.println(numberOfSubjectsPassedInASemester);
+	 numberOfSubjectsPassedInASemester = 5;
+	 System.out.println(numberOfSubjectsPassedInASemester);
+		
+		
+		
+     /// short///
+	 
+	 short numberOfBonesInHuman = 206;
+	 System.out.println(numberOfBonesInHuman);
+	 
+	 short numberOfDaysInAYear = 365;
+	 System.out.println(numberOfDaysInAYear);
+	 
+	 short numberOfSecondsInAHour = 3600;
+	 System.out.println(numberOfSecondsInAHour);
+	 
+	 short priceOfOneLitreCookingOil = 130;
+	 System.out.println(priceOfOneLitreCookingOil);
+	 priceOfOneLitreCookingOil = 129;
+	 System.out.println(priceOfOneLitreCookingOil);
+	 
+	 short numberOfChairsInAuditorium = 160;
+	 System.out.println(numberOfChairsInAuditorium);
+	 numberOfChairsInAuditorium = 200;
+	 System.out.println(numberOfChairsInAuditorium);
+	 
+	 short numberOfCollegesInBangalore = 2087;
+	 System.out.println(numberOfCollegesInBangalore);
+	 
+	 short numberOfEmployeesInACompany = 2000;
+	 System.out.println(numberOfEmployeesInACompany);
+	 numberOfEmployeesInACompany = 2500;
+	 System.out.println(numberOfEmployeesInACompany);
+	 
+	 short employeeSalary = 30000;
+	 System.out.println(employeeSalary);
+	 employeeSalary = 31500;
+	 System.out.println(employeeSalary);
+	 
+	 short courseFee = 20000;
+	 System.out.println(courseFee);
+	 courseFee = 25000;
+	 System.out.println(courseFee);
+	 
+	 short numberOfSeatsInAirplane = 500;
+	 System.out.println(numberOfSeatsInAirplane);
+	 numberOfSeatsInAirplane = 850;
+	 System.out.println(numberOfSeatsInAirplane);
+	 
+	 
+	 
+	 ///int///
+	 
+	 int priceOfLaptop = 60000;
+	 System.out.println(priceOfLaptop);
+	 priceOfLaptop = 55000;
+	 System.out.println(priceOfLaptop);
+	  
+	 int costOfCar = 1200000;
+	 System.out.println(costOfCar);
+	 costOfCar = 2500000;
+	 System.out.println(costOfCar);
+	  
+	 int numberOfTwoWheelersInTown = 1300000;
+	 System.out.println(numberOfTwoWheelersInTown);
+	 numberOfTwoWheelersInTown = 1400000 ;
+	 System.out.println(numberOfTwoWheelersInTown);
+	  
+	 int employeeId = 234516;
+	 System.out.println(employeeId);
+	 employeeId = 345568;
+	 System.out.println(employeeId);
+	  
+	 int numberOfCompaniesInBangalore = 54000;
+	 System.out.println(numberOfCompaniesInBangalore);
+	 numberOfCompaniesInBangalore = 60000;
+	 System.out.println(numberOfCompaniesInBangalore);
+	 
+	 int costOfHouse = 6000000;
+	 System.out.println(costOfHouse);
+	 costOfHouse = 7000000;
+	 System.out.println(costOfHouse);
+	  
+	 int femalePopulationOfIndia = 649500000;
+	 System.out.println(femalePopulationOfIndia);
+	 femalePopulationOfIndia = 649600000;
+	 System.out.println(femalePopulationOfIndia);
+	  
+	 int numberOfPeopleInATwon = 234009000 ;
+	 System.out.println(numberOfPeopleInATwon);
+	  
+	 int totalKannadaSongs = 150600;
+	 System.out.println(totalKannadaSongs);
+	  
+	 int totalTemplesInIndia = 648000;
+	 System.out.println(totalTemplesInIndia);
+	  
+	 int numberOfPrivateSchoolsInIndia = 303000;
+	 System.out.println(numberOfPrivateSchoolsInIndia);
+	  
+	  
+	  
+	 ///long///
+	 
+	 long aadharNumber = 966756782309L;
+	 System.out.println(aadharNumber);
+	 aadharNumber = 945267542130L;
+	 System.out.println(aadharNumber);
+		 
+	 long accountNumber =93456789002L;
+	 System.out.println(accountNumber);
+	 accountNumber=95637890021L;
+	 System.out.println(accountNumber);
+		 
+	 long populationOfIndia = 140000000000L;
+	 System.out.println(populationOfIndia);
+   	 populationOfIndia = 145000000000000L;
+	 System.out.println(populationOfIndia);
+		 
+	 long numberOfSpieces = 9000000000000L;
+	 System.out.println(numberOfSpieces);
+	 numberOfSpieces = 900000000000000L;
+	 System.out.println(numberOfSpieces);
+		 
+	 long phoneNumber = 9008832598L;
+	 System.out.println(phoneNumber);
+	 phoneNumber = 9740462479L;
+	 System.out.println(phoneNumber);
+		 
+	 long numberOfStarsInAGalaxy = 10000000000000L;
+	 System.out.println(numberOfStarsInAGalaxy);
+	 
+	 long numberOfLotteryTicketsSold = 2527892000L;
+	 System.out.println(numberOfLotteryTicketsSold);
+	 
+	 long numberOfTreeInAWorld = 4500000000L;
+	 System.out.println(numberOfTreeInAWorld);
+	 
+	 }
+		
+}
