3a21e4e6499d80d507ff26e9481046f58589d08b
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-14 13:46:08 +0900

    loop

---
+++ Watch.java
@@ -0,0 +1,53 @@
+public class Watch{
+	public static void main(String[] args){
+		byte[] values = {(byte)12 ,(byte)15, (byte)14, (byte)12 ,(byte)122 ,(byte)112 ,(byte)89, (byte)34 ,(byte)90, (byte)34 ,(byte)22 ,(byte)62, (byte)17, (byte)32,(byte)82 ,(byte)93, (byte)45 ,(byte)87 ,(byte)29 , (byte)71};
+		for (int i=0; i<20; i++){
+			System.out.println(values[i]);
+			System.out.println(values[5]);
+		}
+		
+			
+		short[]	numbers = {(short)150,(short)250,  (short)136,  (short)178,  (short)190,  (short)200,  (short)250,  (short)270,  (short)350,  (short)158,  (short)120,  (short)390,  (short)340,  (short)560,  (short)250,  (short)380,  (short)345,  (short)278,  (short)459,  (short)230};
+		for (int j=0; j<20; j++){
+			System.out.println(numbers[j]);
+			System.out.println(numbers[10]);
+		}
+		
+		int[] employeeIds = {23563,98756,93459,56470,20100,56680,34578,18990,37625,47896,12390,61789,54563,13700,78994,37289,45909,56042,40496,12093};
+		for (int k=0; k<20; k++){
+			System.out.println(employeeIds[k]);
+			System.out.println(employeeIds[19]);
+		}
+		
+		long[] aadharNumbers = {123456765432l,786956438907l,345676541234l,678907653456l,124567890987l,123465765980l,456790876543l,564378659087l,789065436789l,654789076543l,234567890987l,658967889087l,890765431234l,567890876543l,345678906432l,123452671234l,567843218907l,908756781234l,786565433456l,980765437890l};
+		for (int index=0; index<20; index++){
+			System.out.println(aadharNumbers[index]);
+			System.out.println(aadharNumbers[15]);
+		}
+		
+		float[] cgpa = { 5.9f,7.8f,6.9f,8.0f,5.9f,8.7f,8.6f,6.9f,7.0f,8.0f,5.8f,7.9f,6.9f,6.0f,7.5f,7.0f,4.9f,4.8f,7.5f,7.9f};
+		for (int m=0; m<20; m++){
+			System.out.println(cgpa[m]);
+		}
+		
+		double[] doubleArr = {9.567843d,4.5432134d,12.3456321d,3.56784d,23.456432d,5.5679084d,45.4567890d,3.4567432d,67.54327d,78.234456789d,23.12345679d,24.345678d,34.4567890d,21.344566d,36.45676543d,28.45678d,38.890754d,45.678654d,45.8765678d,89.765432d};
+		for (int a=0; a<20; a++){
+			System.out.println(doubleArr[a]);
+		}
+		
+		char[] alphapbets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'};
+		for (int p=0; p<20; p++){
+			System.out.println(alphapbets[p]);
+			
+		}
+		
+		boolean[] booleanArray = {true,false,true,false,false,false,true,true,true,true,false,true,true,true,false,false,false,true,true,false};
+		for (int p=0; p<20; p++){
+			System.out.println(booleanArray[p]);
+		}
+			
+		
+		
+		
+	}
+}
