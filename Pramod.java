1bb9c1190b3c51d8585d1dec4cf4cf9939f0e564
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-25 20:32:05 +0900

    dfg

---
+++ Pramod.java
@@ -0,0 +1,19 @@
+public class Pramod
+{
+  public static void main(String[] args)
+  {
+    String[]features = {"sdf","sdfg","koli","opuh"};
+	for (int i=0;i<=0;i++)
+	{
+		System.out.println(features[0]);
+	}
+	
+	
+	for (int i=1;i<=1;i++)
+	{
+		System.out.println(features[1]);
+	}
+   
+   
+  }
+}
