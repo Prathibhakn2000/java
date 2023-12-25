cc72ba140716ceb41f627da608f0115a93dab711
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2023-12-25 21:31:23 +0900

    variables_examples

---
+++ TypeOfVariables.java
@@ -0,0 +1,31 @@
+public class TypeOfVariables
+{
+  static char grade='b';  // instatnce variables
+  public static void main(String[] args)
+  {
+   read(20);
+   run(3.7f);
+  }
+   static void read(int number)
+  {
+    int value=10;       // local variables
+	value=67;
+	grade='c';
+	
+	if(67<10)
+	{
+	 System.out.println("67 greater");
+	}
+	else
+	{ 
+	 System.out.println("10 is greater");
+	}
+  }
+  
+   static void run(float num)
+  {
+	System.out.println(grade);
+	
+  }
+   
+}
