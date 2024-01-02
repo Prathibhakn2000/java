c415093c899c004b4f5bed731cf4053579fdc239
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2024-01-02 22:43:58 +0900

    Patterns

--- Pattern.java
+++ Pattern.java
@@ -36,18 +36,6 @@ public class Pattern
     }
   System.out.println(" ");
   } 
-  
-  
-  System.out.println("---------------------------");
-  for (int i=1;i<=n;i--)
-  {
-	for (int j=1;j<=n;j--)
-	{  
-  
-  System.out.print(" * ");
-    }
-  System.out.println(" ");
-  } 
  
  }
 }
